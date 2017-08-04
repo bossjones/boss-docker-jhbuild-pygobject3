8762333502e3cfbdcea050aa895c6f55083b15f0
Malcolm Jones <malcolm@adobe.com>
2017-08-03 20:19:22 -0400

    Chg: ccache compiler

--- container/root/scripts/jhbuild-compile/01-ccache.compile.sh
+++ container/root/scripts/jhbuild-compile/01-ccache.compile.sh
@@ -2,11 +2,11 @@
 # Configure ccache compiler cache. This shall help speeding up compilation
 echo "****************[ccache]****************" && \
 CCACHE=$(which ccache)
-sudo -u "pi" mkdir -p "/home/pi/jhbuild/install/bin"
+sudo -u "pi" mkdir -p "/home/pi/jhbuild/bin"
 if [ "$CCACHE" ]; then
   for compiler in cc gcc c++ g++; do
-    if [ ! -e "/home/pi/jhbuild/install/bin/$compiler" ]; then
-      sudo -u "pi" ln -s "$CCACHE" "/home/pi/jhbuild/install/bin/$compiler"
+    if [ ! -e "/home/pi/jhbuild/bin/$compiler" ]; then
+      sudo -u "pi" ln -s "$CCACHE" "/home/pi/jhbuild/bin/$compiler"
     fi
   done
 fi
@@ -15,7 +15,7 @@ fi
 
 # enable ccache
 export CCACHE_DIR=/ccache
-export PATH=/usr/lib/ccache:$PATH
+# export PATH=/usr/lib/ccache:$PATH
 
 # TODO: Implent this guy here
 # CCACHE=$(which ccache)
@@ -33,18 +33,17 @@ export PATH=/usr/lib/ccache:$PATH
 # ccache -M 2G
 # (where 2G is the size the cache). Create symlinks to CCache for the compiler in ~/bin:
 
-# mkdir -p ~/bin
-# cd ~/bin
-# for cmd in cc gcc c++ g++; do
-#   ln -s /usr/bin/ccache $cmd
-# done
+mkdir -p /home/pi/bin
+cd /home/pi/bin
+for cmd in cc gcc c++ g++; do
+  ln -s /usr/bin/ccache $cmd
+done
 # It is possible to check the status of the cache including cache hit rates with the following command:
 
 # ccache -s
 
-# export PATH=~/bin:$PATH
-
-# export PATH=/usr/lib/ccache:${PATH}
+export PATH=/home/pi/bin:$PATH
+export PATH=/usr/lib/ccache:${PATH}
 
 #  pi  ⎇  master  ~/gnome/gtk-doc   echo $PATH
 # /usr/lib/ccache:/home/pi/bin:/home/pi/jhbuild/bin:/home/pi/jhbuild/sbin:/home/pi/.local/bin:/home/pi/jhbuild/bin:/home/pi/jhbuild/sbin:/usr/local/bin:/usr/local/sbin:/home/pi/jhbuild/bin:/home/pi/jhbuild/sbin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
